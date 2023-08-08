213be85d74a921257ea53e3e602897c828ede93b
Keks <keks@htmlacademy.ru>
2023-08-08 16:20:06 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/thumbnail.js
@@ -0,0 +1,30 @@
+// Находим шаблон и контейнер
+const thumbnailTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const container = document.querySelector('.pictures');
+
+
+// Получаем свойства атрибутов
+const createThumbnail = ({ comments, description, likes, url, id }) => {
+  const thumbnail = thumbnailTemplate.cloneNode(true);
+
+  thumbnail.querySelector('.picture__img').src = url;
+  thumbnail.querySelector('.picture__img').alt = description;
+  thumbnail.querySelector('.picture__comments').textContent = comments.length;
+  thumbnail.querySelector('.picture__likes').textContent = likes;
+  thumbnail.dataset.thumbnailId = id;
+
+  return thumbnail;
+};
+
+//Cоздаем множество элементов
+const renderThumbnails = (pictures) => {
+  const fragment = document.createDocumentFragment();
+  pictures.forEach((picture) => {
+    const thumbnail = createThumbnail(picture);
+    fragment.append(thumbnail);
+  });
+
+  container.append(fragment);
+};
+
+export { renderThumbnails };
